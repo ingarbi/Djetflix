87cd14f2b646717877014df821a76b2f78d1bd44
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-01-11 14:03:44 +0300

    Added VideoPubProxy and before adding VideoAllproxy

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Video, VideoProxy, VideoAllProxy
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+    # list_display = [ 'video_id']
+
+    class Meta:
+        model = VideoAllProxy
+
+
+admin.site.register(VideoAllProxy, VideoAdmin)
+
+class VideoProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+    # list_display = [ 'video_id']
+
+    def get_queryset(self, request):
+        return VideoProxy.objects.filter(active=True)
+
+    class Meta:
+        model = VideoProxy
+
+
+admin.site.register(VideoProxy, VideoProxyAdmin)
+
--- videos/models.py
+++ videos/models.py
@@ -5,4 +5,20 @@ class Video(models.Model):
     title = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
+
     video_id = models.CharField(max_length=220)
+    active = models.BooleanField(default=True)
+
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
+
+
+class VideoProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
