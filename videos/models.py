e90f63fbdf357854235921f849a2e25df0f00cb2
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-01-11 15:50:39 +0300

    CharField Choices for Publishing Videos and Timestamps

--- videos/models.py
+++ videos/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Video(models.Model):
+    class VideoStateOptions(models.TextChoices):
+        # CONSTANT = DB_VALUE, USER_DISPLAY_VA
+        PUBLISH = 'PU', 'Publish'
+        DRAFT = 'DR', 'Draft'
+        # UNLIST = 'UN', 'Unlist'
+        # PRIVATE = 'PR', 'Private'
+
     title = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
@@ -9,9 +17,20 @@ class Video(models.Model):
     video_id = models.CharField(max_length=220)
     active = models.BooleanField(default=True)
 
+    state = models.CharField(max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
+    publish_timestamp = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True)
+
     @property
     def is_published(self):
         return self.active
+    
+    def save(self, *args, **kwargs):
+        if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:
+            print("saved as timestamp for published")
+            self.publish_timestamp = timezone.now()
+        elif self.state == self.VideoStateOptions.DRAFT:
+            self.publish_timestamp = None
+        super().save(*args, **kwargs)
 
 
 class VideoAllProxy(Video):
