62dbfb31e6003e8c9c3ef1189251fd5c9d33e188
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-01-11 22:27:11 +0300

    Added test for choice anf timestamp

--- videos/tests.py
+++ videos/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils import timezone
 
 from .models import Video
 
@@ -6,6 +7,7 @@ from .models import Video
 class VideoModelTestCase(TestCase):
     def setUp(self):
         Video.objects.create(title='This is my title')
+        Video.objects.create(title='This is my title', state=Video.VideoStateOptions.PUBLISH)
     
     def test_valid_title(self):
         title='This is my title'
@@ -15,5 +17,18 @@ class VideoModelTestCase(TestCase):
     def test_created_count(self):
         title='This is my title'
         qs = Video.objects.filter(title=title)
+        self.assertEqual(qs.count(), 2)
+    
+    def test_draft_case(self):
+        qs = Video.objects.filter(state=Video.VideoStateOptions.DRAFT)
         self.assertEqual(qs.count(), 1)
+    
+    def test_publish_case(self):
+        qs = Video.objects.filter(state=Video.VideoStateOptions.PUBLISH)
+        now = timezone.now()
+        published_qs = Video.objects.filter(
+            publish_timestamp__lte=now,
+            state=Video.VideoStateOptions.PUBLISH
+        )
+        self.assertTrue(published_qs.exists())
 
